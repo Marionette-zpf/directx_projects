d3bd7c83007f955ea0b28a43beb2db4662c4735b
Marionette-zpf <1195810889@qq.com>
2023-08-23 02:21:50 +0800

    添加功能：window class

--- hw3d/hw3d/WinMain.cpp
+++ hw3d/hw3d/WinMain.cpp
@@ -1,86 +1,45 @@
-#include <Windows.h>
-#include "WindowsMessageMap.h"
-#include <sstream>
+/******************************************************************************************
+*	Chili Direct3D Engine																  *
+*	Copyright 2018 PlanetChili <http://www.planetchili.net>								  *
+*																						  *
+*	This file is part of Chili Direct3D Engine.											  *
+*																						  *
+*	Chili Direct3D Engine is free software: you can redistribute it and/or modify		  *
+*	it under the terms of the GNU General Public License as published by				  *
+*	the Free Software Foundation, either version 3 of the License, or					  *
+*	(at your option) any later version.													  *
+*																						  *
+*	The Chili Direct3D Engine is distributed in the hope that it will be useful,		  *
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of						  *
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  *
+*	GNU General Public License for more details.										  *
+*																						  *
+*	You should have received a copy of the GNU General Public License					  *
+*	along with The Chili Direct3D Engine.  If not, see <http://www.gnu.org/licenses/>.    *
+******************************************************************************************/
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) 
-{
-	static WindowsMessageMap mm;
-	OutputDebugString(mm(msg, lParam, wParam).c_str());
-
-	switch (msg)
-	{
-		case WM_CLOSE:
-			PostQuitMessage(69);
-			break;
-		case WM_KEYDOWN:
-			if (wParam == 'F')
-			{
-				SetWindowText(hWnd, "Respects");
-			}
-			break;
-		case WM_KEYUP:
-			if (wParam == 'F')
-			{
-				SetWindowText(hWnd, "Dangerfield");
-			}
-			break;
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
-	HINSTANCE hPreInstance,
+	HINSTANCE hPrevInstance,
 	LPSTR     lpCmdLine,
-	int       nCmdShow
-) 
+	int       nCmdShow)
 {
-	const auto pClassName = "hw3dbutts";
+	Window wnd(800, 300, "Donkey Fart Box");
 
-	// register window class.
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
-
-	// create window instance.
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		"Happw Hard Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-
-	// show the damn window.
-	ShowWindow(hWnd, SW_SHOW);
-
-	// message pump.
 	MSG msg;
 	BOOL gResult;
 	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
 		TranslateMessage(&msg);
-		DispatchMessageA(&msg);
+		DispatchMessage(&msg);
 	}
 
 	if (gResult == -1)
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
+
+	return msg.wParam;
 }
